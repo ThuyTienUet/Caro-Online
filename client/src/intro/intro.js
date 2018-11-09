f3d30d06b7b46c1c2337c9a382e08bd99196f18a
tkqdmd <thaptam0@gmail.com>
2018-11-09 14:50:44 +0700

    update

--- client/src/intro/intro.js
+++
@@ -1,9 +0,0 @@
-angular
-    .module('caroOnline')
-    .controller('introCtrl', introCtrl);
-
-function introCtrl(auth, $location) {
-    if (auth.isLoggedIn() == true) {
-        $location.path('/home')
-    }
-}
