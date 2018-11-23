ba132f0159e54392d82f7f2b69068a112aef254e
tkqdmd <thaptam0@gmail.com>
2018-11-23 09:17:04 +0700

    update

--- client/src/administration/administration.js
+++ client/src/administration/administration.js
@@ -4,4 +4,16 @@ angular
 
 function adminCtrl($scope, $window, $timeout, $http) {
 
+    $scope.users = [];
+    $http.post('/api/user/list')
+        .then(function successCallback(data) {
+            $scope.users = data.data.listUser;
+        }, function errorCallback(err) {
+            console.log(err);
+        })
+
+    $scope.deleteUser = function (id) {
+        console.log(id);
+        
+    }
 }
