126192a8422a0ab3a297b6425c33db10419e94bb
ThuyLinh <rainyboy.165@gmail.com>
2018-12-10 21:32:33 +0700

    update rule

--- server/socket.io/socket.io.js
+++ server/socket.io/socket.io.js
@@ -43,7 +43,7 @@ SOCKET_IO.connect = function (io) {
                 curPlayer: -1
             }
             PLAYER1[data.room.id].username = LIST_USER_OF_ROOM[data.room.id][0];
-            
+
             socket.join(data.room.name);
             io.in(data.room.name).emit('joinedRoom',
                 {
@@ -98,7 +98,7 @@ SOCKET_IO.connect = function (io) {
         });
 
         socket.emit('time', '');
-        
+
         socket.on('startPlay', function (data) {
             if (data.user.username != PLAYER1[data.room.id].username) {
                 if ((PLAYER2[data.room.id].username == '') || (PLAYER2[data.room.id].username == data.user.username)) {
@@ -126,9 +126,9 @@ SOCKET_IO.connect = function (io) {
                     io.in(data.room.name).emit('initBoard', { board: BOARD[data.room.id], player2: PLAYER2[data.room.id], clock: CLOCK[data.room.id] });
                 }
             }
-        }) 
+        })
 
-        socket.on('quitRoom', function (data) { 
+        socket.on('quitRoom', function (data) {
             if (LIST_USER_OF_ROOM[data.room.id]) {
                 LIST_USER_OF_ROOM[data.room.id].forEach(function (username, i) {
                     if (username == data.user.username) {
@@ -171,7 +171,7 @@ SOCKET_IO.connect = function (io) {
         })
 
         socket.on('cancelRoom', function (data) {
-            io.in(data.name).emit('cancelledRoom', ''); 
+            io.in(data.name).emit('cancelledRoom', '');
         })
 
         socket.on('register', function (data) {
@@ -180,7 +180,7 @@ SOCKET_IO.connect = function (io) {
 
         socket.on('deleteUser', function (data) {
             console.log(data);
-            
+
             io.emit('cancelUser', data);
         })
     })
@@ -202,15 +202,11 @@ Array.matrix = function (n, init) {
 function checkOnHorizontal(board, cur_row, cur_col, cur_val) {
     let count_left = 0;
     let count_right = 0;
-    let isBlocked = 0;
     //di sang trai so voi cur_pos
     for (let i = cur_col; i >= 0; i--) {
         if (board[cur_row][i] === cur_val) {
             count_left++;
         } else {
-            if (board[cur_row][i] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
@@ -219,14 +215,11 @@ function checkOnHorizontal(board, cur_row, cur_col, cur_val) {
         if (board[cur_row][i] === cur_val) {
             count_right++;
         } else {
-            if (board[cur_row][i] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
 
-    if ((count_left + count_right === 4 && isBlocked === 0) || (count_left + count_right >= 5)) {
+    if (count_left + count_right >= 5) {
         return 1;
     }
 }
@@ -235,14 +228,10 @@ function checkOnHorizontal(board, cur_row, cur_col, cur_val) {
 function checkOnVertically(array_board, cur_row, cur_col, cur_val) {
     let count_up = 0;
     let count_down = 0;
-    let isBlocked = 0;
     for (let i = cur_row; i < 15; i++) {
         if (array_board[i][cur_col] === cur_val) {
             count_down++;
         } else {
-            if (array_board[i][cur_col] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
@@ -250,13 +239,10 @@ function checkOnVertically(array_board, cur_row, cur_col, cur_val) {
         if (array_board[i][cur_col] === cur_val) {
             count_up++;
         } else {
-            if (array_board[i][cur_col] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
-    if ((count_up + count_down === 4 && isBlocked === 0) || (count_up + count_down >= 5))  {
+    if (count_up + count_down >= 5) {
         return 1;
     }
 }
@@ -265,7 +251,6 @@ function checkOnVertically(array_board, cur_row, cur_col, cur_val) {
 function checkOnDiagonal(array_board, cur_row, cur_col, cur_val) {
     let count_right_up = 0;
     let count_left_down = 0;
-    let isBlocked = 0;
     let temp1 = 0;
     let temp2 = 1;
     //kiem tra theo duong cheo phia tren ben phai so voi cur_pos
@@ -274,9 +259,6 @@ function checkOnDiagonal(array_board, cur_row, cur_col, cur_val) {
             count_right_up++;
             temp1++;
         } else {
-            if (array_board[i][cur_col + temp1] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
@@ -286,13 +268,10 @@ function checkOnDiagonal(array_board, cur_row, cur_col, cur_val) {
             count_left_down++;
             temp2++;
         } else {
-            if (array_board[i][cur_col - temp2] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
-    if ((count_left_down + count_right_up === 4 && isBlocked === 0) || (count_left_down + count_right_up >= 5)) {
+    if (count_left_down + count_right_up >= 5) {
         return 1;
     }
 }
@@ -301,7 +280,6 @@ function checkOnDiagonal(array_board, cur_row, cur_col, cur_val) {
 function checkOnDiagonalMain(array_board, cur_row, cur_col, cur_val) {
     let count_right_down = 0;
     let count_left_up = 0;
-    let isBlocked = 0;
     let temp1 = 0;
     let temp2 = 1;
     //kiem tra phia tren ben trai cua cur_pos
@@ -310,10 +288,7 @@ function checkOnDiagonalMain(array_board, cur_row, cur_col, cur_val) {
             count_left_up++;
             temp1++;
         } else {
-            if (array_board[i][cur_col - temp1] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
-             break;
+            break;
         }
     }
     //kiem tra phia duoi ben trai so voi cur_pos
@@ -322,13 +297,10 @@ function checkOnDiagonalMain(array_board, cur_row, cur_col, cur_val) {
             count_right_down++;
             temp2++;
         } else {
-            if (array_board[i][cur_col + temp2] === cur_val*(-1)) {
-                isBlocked = 1;
-            }
             break;
         }
     }
-    if ((count_left_up + count_right_down === 4 && isBlocked === 0) || (count_left_up + count_right_down >= 5)) {
+    if (count_left_up + count_right_down >= 5) {
         return 1;
     }
 }
